58547dde45328ceb86f8ddb00e30c05574a5e70a
Roman <romanbondarenko14@gmail.com>
2019-11-24 21:31:27 +0200

    update nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def application
-    @user_var = 'admin'
+  helper_method :get_current_user_name
+
+  def get_current_user_name
+    if current_user
+      "#{current_user.first_name} #{current_user.second_name}"
+    end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @user_name = "test"
   end
 end
