f98e6cdb429e505438902fcb770ba294e80c497c
Roman <romanbondarenko14@gmail.com>
2019-11-25 13:36:24 +0200

    add user and admin as resources

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,4 @@
 class AdminController < ApplicationController
-  def index
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'admin/index'
+  resources 'user'
+  resources 'admin'
   devise_for :users, controllers: {registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
