f03c30e0abf27c266313a6246a351922fdd2b3a9
Roman <romanbondarenko14@gmail.com>
2019-11-25 12:54:55 +0200

    add user and admin controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'admin/index'
   devise_for :users, controllers: {registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
