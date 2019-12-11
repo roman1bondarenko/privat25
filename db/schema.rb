3cfb06d15a37912d0e191467d8befb0f56be8330
Roman <romanbondarenko14@gmail.com>
2019-12-11 17:00:10 +0200

    remove city short name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_134930) do
+ActiveRecord::Schema.define(version: 2019_12_11_145849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_12_11_134930) do
   create_table "cities", force: :cascade do |t|
     t.bigint "country_id"
     t.text "name"
-    t.text "short_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["country_id"], name: "index_cities_on_country_id"
