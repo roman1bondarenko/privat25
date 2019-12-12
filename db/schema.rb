74d9d8d66e098f1a4f3c9f0b19cc9ffd6a9fc86e
Roman <romanbondarenko14@gmail.com>
2019-12-12 22:28:58 +0200

    add phone to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_201335) do
+ActiveRecord::Schema.define(version: 2019_12_12_202819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_201335) do
     t.date "birthday"
     t.boolean "is_deleted"
     t.boolean "is_blocked"
+    t.text "phone"
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
