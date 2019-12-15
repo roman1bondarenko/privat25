d7e2ad8cb1312e6df438bd6ffdb2aafde9ba968c
Roman <romanbondarenko14@gmail.com>
2019-12-15 16:24:45 +0200

    add home controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get RU root" do
+    get '/ru'
     assert_response :success
   end
 
+  test "should get EN root" do
+    get '/en'
+    assert_response :success
+  end
+
+  test "should redirect to default locale" do
+    get '/'
+    assert_response :redirect
+  end
 end
