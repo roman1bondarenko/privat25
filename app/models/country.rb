c71aa09d7180a9e2b2213768349cbf40fc93bc0b
Roman <romanbondarenko14@gmail.com>
2019-12-16 17:46:38 +0200

    fix country validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class Country < ApplicationRecord
   has_many :cities
 
   validates_associated :cities
   validates :name, :phone_code, :short_code, presence: true
-  validates :phone_code, format: { with: /[+][0-9]+/ }
+  validates :phone_code, format: { with: /\A\d+\z/,
+                                   message: 'Phone code may contain only numbers' }
 end
