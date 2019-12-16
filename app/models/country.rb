78cdfb0dbc34933ab4c26fd69072ef04636781c8
Roman <romanbondarenko14@gmail.com>
2019-12-16 16:31:36 +0200

    add country validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,7 @@
 class Country < ApplicationRecord
   has_many :cities
+
+  validates_associated :cities
+  validates :name, :phone_code, :short_code, presence: true
+  validates :phone_code, format: { with: /[+][0-9]+/ }
 end
