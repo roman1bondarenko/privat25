68526fdfcde6cafc69145e21d085d1e2f1f11e2c
Roman <romanbondarenko14@gmail.com>
2019-12-16 17:47:46 +0200

    add country model test

--- test/models/country_test.rb
+++ test/models/country_test.rb
@@ -1,7 +1,25 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CountryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid country' do
+    country = Country.create(name: 'Ukraine', short_code: 'UA', phone_code: '380')
+    assert country.valid?
+  end
+
+  test 'should be invalid country' do
+    params = [
+      { name: 'USA' },
+      { name: 'USA', phone_code: '1d' },
+      { phone_code: '1', short_code: 'US' },
+      { name: 'USA', phone_code: '12ds', short_code: 'US' }
+    ]
+    params.each do |param|
+      country = Country.create(name: param[:name],
+                               short_code: param[:short_code],
+                               phone_code: param[:phone_code])
+      assert country.invalid?
+    end
+  end
 end
