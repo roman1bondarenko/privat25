d135acd8feda0a82429a68e522c4ac8038fb12b9
Roman <romanbondarenko14@gmail.com>
2019-12-16 17:58:42 +0200

    add city model test

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,23 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid city' do
+    country = Country.create(name: 'Ukraine', short_code: 'UA', phone_code: '380')
+    city = City.create(name: 'Krop', country: country)
+    assert city.valid?
+  end
+
+  test 'should be invalid city' do
+    country = Country.create(name: 'Ukraine', short_code: 'UA', phone_code: '380')
+    params = [
+      { name: 'Krop' },
+      { country: country }
+    ]
+    params.each do |param|
+      city = City.create(name: param[:name], country: param[:country])
+      assert city.invalid?
+    end
+  end
 end
