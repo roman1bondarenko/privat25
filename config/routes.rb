bbdd36c6da30cd6cd8c7a16606cddeb2be473b8c
Roman <romanbondarenko14@gmail.com>
2019-12-17 16:32:13 +0200

    add manager model and manager device views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope ':locale', locale: /#{I18n.available_locales.join("|")}/ do
     resources 'user'
     devise_for :users, controllers: { registrations: 'registrations' }
+    devise_for :managers
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
   resources :cities, only: :index
