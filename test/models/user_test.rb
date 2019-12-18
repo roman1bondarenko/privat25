99fa9c600f02ad5877b265eaaa2b168735beb983
Roman <romanbondarenko14@gmail.com>
2019-12-18 13:09:43 +0200

    refactored test DRY

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  user_params =
+    { first_name: 'Roman', second_name: 'Bondarenko', email: 'mymail@gmail.com',
+      gender: User.genders[:male], birthday: '1998-12-03', phone: '095782341',
+      password: 'password', city: City.first.id }
+
   test 'should be valid user ' do
     params = [
       { first_name: 'Roman', second_name: 'Bondarenko', email: 'mymail@gmail.com',
@@ -19,11 +24,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'should be invalid user if missing param' do
-    user_params =
-      { first_name: 'Roman', second_name: 'Bondarenko', email: 'mymail@gmail.com',
-        gender: User.genders[:male], birthday: '1998-12-03', phone: '095782341',
-        password: 'password', city: City.first.id }
-
     # gender dont need to remove because by default it set 0 if missing
     params_to_remove = %i[first_name second_name email birthday phone password city]
     params_to_remove.each do |param|
@@ -33,20 +33,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'should be invalid user when incorrect email and phone' do
-    param = { first_name: 'Roman', second_name: 'Bondarenko', email: nil,
-              gender: User.genders[:male], birthday: '1998-12-03', phone: '095782341',
-              password: 'password', city: City.first.id }
     emails = %w[my@ @ @mail.com my@mail.1- mail]
     phones = %w[1234567q q qqq q1234567 @12345]
-    test_contact_values(param, 'email', emails)
-    test_contact_values(param, 'phone', phones)
-  end
-end
-
-def test_contact_values(user_params, contact_cell_name, data_to_test)
-  data_to_test.each do |data|
-    user_params[contact_cell_name] = data
-    user = User.create(user_params)
-    assert user.invalid?
+    test_contact_values(User, user_params, 'email', emails)
+    test_contact_values(User, user_params, 'phone', phones)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -6,5 +8,20 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  protected
+
+  def test_missing_params(model, params, params_to_delete)
+    params_to_delete.each do |param|
+      model_obj = model.create(params.except(param))
+      assert model_obj.invalid?
+    end
+  end
+
+  def test_contact_values(model, params, contact_cell_name, data_to_test)
+    data_to_test.each do |data|
+      params[contact_cell_name] = data
+      model_obj = model.create(params)
+      assert model_obj.invalid?
+    end
+  end
 end
