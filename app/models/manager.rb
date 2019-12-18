2387c5bad536f32ee102d30c671bcb01a1dd4908
Roman <romanbondarenko14@gmail.com>
2019-12-18 13:09:20 +0200

    add manager model tests

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class Manager < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :second_name, :email, presence: true
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- test/models/manager_test.rb
+++ test/models/manager_test.rb
@@ -1,7 +1,24 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ManagerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  params = { first_name: 'Roman', second_name: 'Bondarenko',
+             email: 'managermail@gmail.com', password: 'password' }
+
+  test 'should create and be a valid manager ' do
+    params[:email] = 'newmanagermail@gmail.com'
+    manager = Manager.create(params)
+    assert manager.valid?
+  end
+
+  test 'should be invalid manager if missing required param' do
+    params_to_delete = %i[first_name second_name email password]
+    test_missing_params(Manager, params, params_to_delete)
+  end
+
+  test 'should be invalid when incorrect email' do
+    emails = %w[my@ @ @mail.com my@mail.1- mail]
+    test_contact_values(Manager, params, 'email', emails)
+  end
 end
