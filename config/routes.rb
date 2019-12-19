3128af7a8e11372c19d795475da617e4b96669ab
Roman <romanbondarenko14@gmail.com>
2019-12-19 14:38:40 +0200

    add managers controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resources 'user'
     devise_for :users, path: 'users',
                        controllers: { registrations: 'users/registrations' }
-    devise_for :managers
+    devise_for :managers, path: 'managers',
+                          controllers: { registrations: 'managers/registrations',
+                                         sessions: 'managers/sessions' }
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
   resources :cities, only: :index
